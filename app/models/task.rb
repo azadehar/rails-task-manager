69de31c35b1b2124af8f5fdc849149f2c1d3f43a
azadeharam <aram.azadeh@yahoo.com>
2020-02-18 16:33:19 +0100

    Task Manager

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,2 @@
 class Task < ApplicationRecord
-  def change
-    create_table :tasks do |t|
-      t.string :title
-      t.details :text
-      t.completed :false
-
-      t.timestamps
-    end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  # get 'tasks/title', to "tasks#index"
+  # get 'tasks/details'
+  # get 'tasks/completed'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    "tasks",  to: "tasks#index"
+
+  get    "tasks/new",      to: "tasks#new" , as: :new_task
+  post   "tasks",          to: "tasks#create"
+
+  # NB: The `show` route needs to be *after* `new` route.
+  get    "tasks/:id",      to: "tasks#show", as: :task
+
+  get    "tasks/:id/edit", to: "tasks#edit"
+  patch  "tasks/:id",      to: "tasks#update"
+
+  delete "tasks/:id",      to: "tasks#destroy"
 end
